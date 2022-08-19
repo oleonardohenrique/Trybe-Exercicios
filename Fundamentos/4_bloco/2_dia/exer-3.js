3b2d38f375719adac9539a11715b6b4687034b42
Leonardo <leoohenriquesousa@gmail.com>
2022-08-19 18:15:32 -0300

    Remove

--- Fundamentos/4_bloco/2_dia/exer-3.js
+++
@@ -1,9 +0,0 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let tot = 0;
-let div = 0;
-
-for (let index = 0; index < numbers.length; index += 1) {
-    tot += numbers[index]
-}
-
-console.log(tot / numbers.length)

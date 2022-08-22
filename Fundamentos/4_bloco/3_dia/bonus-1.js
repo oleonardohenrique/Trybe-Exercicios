fdc3483321f2f22542928d8ac5ec2c188252a732
Leonardo <leoohenriquesousa@gmail.com>
2022-08-22 14:55:56 -0300

    Cria um quadrado de asteristicos

---
+++ Fundamentos/4_bloco/3_dia/bonus-1.js
@@ -0,0 +1,10 @@
+let numero = 5;
+let simbolo = '*';
+let linha  = ''
+
+for (let index = 0; index < numero; index += 1) {
+    linha = simbolo + linha
+}
+for (let index = 0; index < numero; index += 1) {
+    console.log(linha)
+}

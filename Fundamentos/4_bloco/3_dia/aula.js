bcf41207455a35d425bd2886e5e92848eb0de9f3
Leonardo <leoohenriquesousa@gmail.com>
2022-08-22 19:45:03 -0300

    Exercício feito durante a aula, o maior objetivo era a interação entre as pessoas

--- Fundamentos/4_bloco/3_dia/aula.js
+++ Fundamentos/4_bloco/3_dia/aula.js
@@ -1,6 +1,5 @@
 let tot = 0;
-for (let index = 1; index < 50; index += 1) {
+for (let index = 1; index <= 50; index += 1) {
     tot += tot = index
 }
-
-console.log(`A soma total de 1 a 50 é: ${tot}`)
+console.log(tot)
